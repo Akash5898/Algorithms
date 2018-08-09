47048fec0ad85bfa54544de7fcc68e4764338951
Akash5898 <42235941+Akash5898@users.noreply.github.com>
2018-08-09 19:29:11 +0530

    Create numberreverse.cpp

---
+++ numberreverse.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int num, rev=0, rem;
+	cout<<"Enter a number : ";
+	cin>>num;
+	while(num!=0)
+	{
+		rem=num%10;
+		rev=rev*10+rem;
+		num=num/10;
+	}
+	cout<<"Reverse = "<<rev;
+	getch();
+}
